5be13e96fab5e52b438bcb06d8ec9ee05e4ccb19
sisuok <sisusisuok@gmail.com>
2026-02-12 07:44:36 +0500

    Initialize Actix web server with static file serving

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use actix_files::Files;
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Chess.World server running on 0.0.0.0:8080");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/", "./static").index_file("index.html"))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
+}
